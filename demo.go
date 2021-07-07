4f9c0e871d0e0db4e1e03ce2900a1f36e517d48f
matthung0807 <bronx0807@gmail.com>
2021-07-07 17:06:55 +0800

    Go Gin Hello World

--- demo.go
+++ demo.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
 
 func main() {
-	fmt.Println("Hello World")
+	r := gin.Default()
+	r.GET("/hello", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "hello world",
+		})
+	})
+	r.Run()
 }
