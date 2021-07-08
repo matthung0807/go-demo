20ec7aa9ab931efc83d7f3cc5d795ddcacb9cb8c
matthung0807 <bronx0807@gmail.com>
2021-07-08 09:41:05 +0800

    Go Gin write log

--- demo.go
+++ demo.go
@@ -1,9 +1,19 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"io"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	router := gin.Default()
+
+	gin.DisableConsoleColor()
+	file, _ := os.Create("gin.log")                     // create log file
+	gin.DefaultWriter = io.MultiWriter(file, os.Stdout) // wirte log to file and console
+
+	router := gin.Default() // with logger attached
 	router.GET("/hello", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "hello world",
