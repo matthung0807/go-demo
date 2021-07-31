f3fb23b424b773cd995fb48c4f190d557fcf6f05
matthung0807 <bronx0807@gmail.com>
2021-07-31 21:58:15 +0800

    Go Gin Group Routes example

--- demo.go
+++ demo.go
@@ -4,10 +4,24 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	router := gin.Default()
-	router.GET("/hello", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "hello world",
+
+	demo := router.Group("/demo")
+
+	demo.Group("/v1").
+		GET("/hello", func(c *gin.Context) {
+			c.JSON(200, "Hello - v1")
+		}).
+		GET("/hi", func(c *gin.Context) {
+			c.JSON(200, "Hi - v1")
 		})
-	})
+
+	demo.Group("/v2").
+		GET("/hello", func(c *gin.Context) {
+			c.JSON(200, "Hello - v2")
+		}).
+		GET("/hi", func(c *gin.Context) {
+			c.JSON(200, "Hi - v2")
+		})
+
 	router.Run()
 }
