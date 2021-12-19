d8bce6aa06db5bab8189637eb448b969fabb9622
matthung0807 <bronx0807@gmail.com>
2021-12-19 22:56:19 +0800

    go static credentials access s3 bucket

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # go-demo
-Go Demo
+[Golang 從本機存取AWS S3 bucket](https://matthung0807.blogspot.com/2021/12/go-localhost-access-aws-s3-bucket.html)
--- main.go
+++ main.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+const (
+	accessKeyID = "AKIAIOSFODNN7EXAMPLE"
+	secretKey   = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	ctx := context.TODO()
+
+	client := NewS3Client(ctx) // Create an Amazon S3 service client
+
+	bucketName := "s3-demo-bucket-202112151320"
+	output := GetBucketObjectOutput(ctx, client, bucketName)
+
+	for _, object := range output.Contents {
+		fmt.Printf("key=%s\n", aws.ToString(object.Key))
+	}
+}
+
+func NewS3Client(ctx context.Context) *s3.Client {
+	creds := aws.NewCredentialsCache(
+		credentials.NewStaticCredentialsProvider(accessKeyID, secretKey, ""))
+
+	cfg, err := config.LoadDefaultConfig(
+		ctx,
+		config.WithRegion("ap-northeast-1"),
+		config.WithCredentialsProvider(creds),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	return s3.NewFromConfig(cfg) // Create an Amazon S3 service client
+}
+
+func GetBucketObjectOutput(
+	ctx context.Context,
+	client *s3.Client,
+	bucketName string) *s3.ListObjectsV2Output {
+
+	output, err := client.ListObjectsV2(ctx, &s3.ListObjectsV2Input{
+		Bucket: aws.String(bucketName),
+	})
+	if err != nil {
+		panic(err)
+	}
+	return output
 }
