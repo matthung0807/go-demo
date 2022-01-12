1d7c6730c64f3ed44bc7897ec9ae1ca812a8b0db
matthung0807 <bronx0807@gmail.com>
2022-01-12 23:14:26 +0800

    go sql query postgresql

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module abc.com/demo
 
 go 1.17
 
-require (
-	github.com/google/uuid v1.3.0
-	github.com/lib/pq v1.10.4
-)
+require github.com/lib/pq v1.10.4
--- main.go
+++ main.go
@@ -6,21 +6,43 @@ import (
 	"log"
 	"time"
 
-	"github.com/google/uuid"
 	_ "github.com/lib/pq"
 )
 
 type Employee struct {
-	ID        uuid.UUID
+	ID        int64
 	Name      string
 	Age       int
 	CreatedOn time.Time
+}
+
+const (
+	HOST     = "localhost"
+	PORT     = "5432"
+	DATABASE = "postgres"
+	USER     = "admin" // 使用者名稱
+	PASSWORD = "12345"
+	SSL      = "disable"
+)
+
+func connect() *sql.DB {
+	driver := "postgres"
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		HOST, PORT, USER, PASSWORD, DATABASE, SSL)
+
+	db, err := sql.Open(driver, dsn)
+	if err != nil {
+		log.Panic("open database error")
+	}
+	return db
 }
 
 func main() {
 	db := connect()
 
 	rows, err := db.Query("SELECT id, name, age, created_on FROM employee")
+	defer rows.Close()
 	if err != nil {
 		panic(err)
 	}
@@ -35,26 +57,5 @@ func main() {
 		employees = append(employees, e)
 	}
 
-	fmt.Println(employees) // f3f6148e-1ac3-4ae4-b96e-16f937393a10
-}
-
-const (
-	HOST     = "localhost"
-	PORT     = "5432"
-	DATABASE = "postgres"
-	USER     = "matt" // 使用者名稱
-	SSL      = "disable"
-)
-
-func connect() *sql.DB {
-	driver := "postgres"
-	dsn := fmt.Sprintf(
-		"host=%s port=%s user=%s dbname=%s sslmode=%s",
-		HOST, PORT, USER, DATABASE, SSL)
-
-	db, err := sql.Open(driver, dsn)
-	if err != nil {
-		log.Panic("open database error")
-	}
-	return db
+	fmt.Println(employees)
 }
