d5de50440d186fb888b8e4dbc7d6b49eff10cba2
matthung0807 <bronx0807@gmail.com>
2022-01-20 20:57:18 +0800

    callback func

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+// hof is Higher Order Function
+func hof(s string, callback func(string)) {
+	fmt.Println(s)
+	callback(s)
+}
+
+// callback is Callback Function
+func callback(s string) {
+	fmt.Printf("callback: %s\n", s)
+}
 
 func main() {
-	fmt.Println("Hello World")
+	hof("hi", callback)
 }
