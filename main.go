281c162785a01414a7cd737de4e46c97146bb8f5
matthung0807 <bronx0807@gmail.com>
2022-01-25 15:17:09 +0800

    go web server

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	http.HandleFunc("/hello", func(rw http.ResponseWriter, r *http.Request) {
+		name := r.URL.Query().Get("name") // get URL query string
+		content := fmt.Sprintf("hello, %s", name)
+		fmt.Fprint(rw, content) // write out content
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
