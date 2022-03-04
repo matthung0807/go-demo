7ecc97ad6bf0b80a4875ca595acf29fddebfacbd
matthung0807 <bronx0807@gmail.com>
2022-03-05 00:05:43 +0800

    go openapi swagger-ui

--- main.go
+++ main.go
@@ -3,24 +3,17 @@ package main
 import (
 	"fmt"
 	"net/http"
-
-	_ "abc.com/demo/docs"
-	httpSwagger "github.com/swaggo/http-swagger"
 )
 
-// @title Swagger Demo
-// @version 1.0
-// @description Swagger API.
-// @host localhost:8080
 func main() {
 	http.HandleFunc("/hello", HelloHandler)
-	http.HandleFunc("/swagger/", httpSwagger.WrapHandler)
+
+	fs := http.FileServer(http.Dir("./swaggerui"))
+	http.Handle("/swagger/", http.StripPrefix("/swagger/", fs))
 
 	http.ListenAndServe(":8080", nil)
 }
 
-// @Success 200 {string} string
-// @Router /demo/hello [get]
 func HelloHandler(rw http.ResponseWriter, r *http.Request) {
 	name := r.URL.Query().Get("name")
 	content := fmt.Sprintf("hello, %s", name)
