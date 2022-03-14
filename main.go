cbb7bfa16f929b3997aa4ea557fca2b2a7840207
matthung0807 <bronx0807@gmail.com>
2022-03-14 21:53:39 +0800

    goroutine send record to firehose

--- main.go
+++ main.go
@@ -1,16 +1,80 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"sync/atomic"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/firehose"
+	"github.com/aws/aws-sdk-go-v2/service/firehose/types"
 )
 
+type Employee struct {
+	ID   int64
+	Name string
+	Age  int
+}
+
 func main() {
-	http.HandleFunc("/hello", func(rw http.ResponseWriter, r *http.Request) {
-		name := r.URL.Query().Get("name") // get URL query string
-		content := fmt.Sprintf("hello, %s", name)
-		fmt.Fprint(rw, content) // write out content
+	http.HandleFunc("/employee", func(rw http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			var emp Employee
+			decoder := json.NewDecoder(r.Body)
+			err := decoder.Decode(&emp)
+			if err != nil {
+				http.Error(rw, "error", http.StatusBadRequest)
+			}
+
+			id := Save(&emp)
+
+			data := fmt.Sprintf("employee.id=%d created\n", id)
+			go PutRecord(context.TODO(), data)
+
+			fmt.Fprint(rw, id)
+		} else {
+			http.Error(rw, "not allowed", http.StatusMethodNotAllowed)
+		}
 	})
 
 	http.ListenAndServe(":8080", nil)
+}
+
+var count int64
+
+func Save(emp *Employee) int64 {
+	fmt.Printf("save employee=%v\n", emp)
+	return atomic.AddInt64(&count, 1)
+}
+
+func PutRecord(ctx context.Context, data string) {
+	fmt.Printf("put data=%s\n", data)
+	cfg, err := config.LoadDefaultConfig(
+		ctx,
+		config.WithRegion("ap-northeast-1"),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	client := firehose.NewFromConfig(cfg)
+
+	input := CreateInput(data)
+	out, err := client.PutRecord(ctx, input)
+	if err != nil {
+		fmt.Println("put record error")
+	}
+	fmt.Printf("recordId=%s\n", *out.RecordId)
+}
+
+func CreateInput(data string) *firehose.PutRecordInput {
+	deliveryStream := "PUT-s3-demo-bucket-202112151320"
+	return &firehose.PutRecordInput{
+		DeliveryStreamName: &deliveryStream,
+		Record: &types.Record{
+			Data: []byte(data),
+		},
+	}
 }
