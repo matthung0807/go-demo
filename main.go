c49fc00c2c2cf024a1bb9e83c81f5627bd2aa35e
matthung0807 <bronx0807@gmail.com>
2022-03-25 23:00:42 +0800

    struct and json conversion

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+)
+
+type Employee struct {
+	ID        int64     `json:"id"`
+	Name      string    `json:"name"`
+	Age       int       `json:"age"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
+var s string = `
+{
+  "id": 1,
+  "name": "john",
+  "age": 33,
+  "createdAt": "2022-01-19T12:34:56Z"
+}`
 
 func main() {
-	fmt.Println("Hello World")
+	var emp Employee
+	json.Unmarshal([]byte(s), &emp) // json to struct
+	fmt.Println(emp)
+
+	b, err := json.Marshal(emp) // struct to json
+	if err != nil {
+		panic(err)
+	}
+	s := string(b)
+	fmt.Println(s)
 }
