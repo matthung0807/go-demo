25d956b7119857ce30d4f7057586257ff58057d1
matthung0807 <bronx0807@gmail.com>
2022-03-31 00:56:40 +0800

    localhost go send message to SQS queue

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/sqs"
+)
 
 func main() {
-	fmt.Println("Hello World")
+
+	ctx := context.TODO()
+	cfg, err := config.LoadDefaultConfig(
+		ctx,
+		config.WithRegion("ap-northeast-1"),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	client := sqs.NewFromConfig(cfg)
+
+	qn := "DemoQueue"
+	urlInput := &sqs.GetQueueUrlInput{
+		QueueName: &qn,
+	}
+
+	urlOutput, err := client.GetQueueUrl(ctx, urlInput)
+	if err != nil {
+		panic(err)
+	}
+
+	msg := "hello world from go"
+	smInput := &sqs.SendMessageInput{
+		DelaySeconds: 0,
+		MessageBody:  &msg,
+		QueueUrl:     urlOutput.QueueUrl,
+	}
+
+	smOutput, err := client.SendMessage(ctx, smInput)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(*smOutput.MessageId)
 }
