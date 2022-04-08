4e8548f933278cdcc1cd8a8524ee513ed5ba9091
matthung0807 <bronx0807@gmail.com>
2022-04-08 18:22:36 +0800

    go generic

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # go-demo
-Go Demo
+[Go 泛型簡介 generics intro](https://matthung0807.blogspot.com/2022/04/go-generics-intro.html)
--- main.go
+++ main.go
@@ -2,6 +2,51 @@ package main
 
 import "fmt"
 
+type Num interface {
+	int | float64 // type constraints
+}
+
+type Nums[T Num] []T // slice of Num
+
 func main() {
-	fmt.Println("Hello World")
+	// call generic function with type arguments
+	fmt.Println(Add[int](1, 2))         // 3
+	fmt.Println(Add[float64](1.0, 2.2)) // 3.2
+	// call generic function without type arguments
+	fmt.Println(Add(1, 2))     // 3
+	fmt.Println(Add(1.0, 2.2)) // 3.2
+
+	var ints Nums[int] = []int{1, 2, 3}
+	PrintAll[int](ints)
+
+	var floats Nums[float64] = []float64{1.1, 2.2, 3.3}
+	PrintAll(floats)
+
+	m := map[string]Nums[int]{
+		"a": {1, 2},
+		"b": {3, 4},
+	}
+
+	PrintMap(m)
+
+}
+
+// func Add[T int | float64](x, y T) N {
+// 	return x + y
+// }
+
+func Add[T Num](x, y T) T {
+	return x + y
+}
+
+func PrintAll[T Num](nums Nums[T]) {
+	for _, n := range nums {
+		fmt.Println(n)
+	}
+}
+
+func PrintMap[K comparable, T Num](m map[K]Nums[T]) {
+	for k, v := range m {
+		fmt.Printf("key=%v, value=%v\n", k, v)
+	}
 }
