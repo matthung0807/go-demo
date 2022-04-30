11977f46f1f5391d4b671dbd2aa6c12297a7dbad
matthung0807 <bronx0807@gmail.com>
2022-04-30 16:25:25 +0800

    concurrent map simple

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # go-demo
-Go Demo
+[Golang concurrent map簡單實作](https://matthung0807.blogspot.com/2022/04/go-concurrent-map-simple.html)
--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var rwmu sync.RWMutex
+
+var m = map[int]int{}
 
 func main() {
-	fmt.Println("Hello World")
+	for i := 0; i < 10; i++ {
+		go put(i, i)
+		go func(k int) {
+			fmt.Println(get(k))
+		}(i)
+	}
+	time.Sleep(time.Second)
+}
+
+func get(k int) int {
+	rwmu.RLock()
+	defer rwmu.RUnlock()
+	v, ok := m[k]
+	if ok == false {
+		return -1
+	}
+	return v
+}
+
+func put(k int, v int) {
+	rwmu.Lock()
+	defer rwmu.Unlock()
+	m[k] = v
+}
+
+func size() int {
+	rwmu.RLock()
+	defer rwmu.RUnlock()
+	return len(m)
 }
