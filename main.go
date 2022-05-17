a30e1b85c1635e36d4aa70d536bdb0c37589843b
matthung0807 <bronx0807@gmail.com>
2022-05-17 21:22:27 +0800

    modify

--- main.go
+++ main.go
@@ -34,11 +34,83 @@ func OpenDB(ctx context.Context) *sql.DB {
 
 func main() {
 	ctx := context.Background()
+	emps, err := GetAll(ctx)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(emps)
+}
+
+func GetAll(ctx context.Context) ([]sqlcDb.Employee, error) {
 	db := OpenDB(ctx)
 	queries := sqlcDb.New(db)
-	employees, err := queries.GetAll(ctx)
+	emps, err := queries.GetAll(ctx)
 	if err != nil {
-		panic("query employee error")
+		return nil, err
 	}
-	fmt.Println(employees)
+	return emps, nil
+}
+
+func GetById(ctx context.Context, id int64) (*sqlcDb.Employee, error) {
+	db := OpenDB(ctx)
+	qr := sqlcDb.New(db)
+	emp, err := qr.GetById(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+	return &emp, nil
+}
+
+func Insert(ctx context.Context, name string, age int) error {
+	db := OpenDB(ctx)
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	params := sqlcDb.InsertParams{
+		Name: name,
+		Age:  sql.NullInt32{int32(age), true},
+	}
+	qr := sqlcDb.New(tx)
+	err = qr.Insert(ctx, params)
+	if err != nil {
+		return err
+	}
+
+	tx.Commit()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func Update(
+	ctx context.Context,
+	id int64, name string, age int) (int64, error) {
+
+	db := OpenDB(ctx)
+	tx, err := db.Begin()
+	if err != nil {
+		return 0, err
+	}
+	defer tx.Rollback()
+
+	params := sqlcDb.UpdateParams{
+		ID:   id,
+		Name: name,
+		Age:  sql.NullInt32{int32(33), true},
+	}
+	qr := sqlcDb.New(tx)
+	rows, err := qr.Update(ctx, params)
+	if err != nil || rows == 1 {
+		return 0, err
+	}
+
+	tx.Commit()
+	if err != nil {
+		return 0, err
+	}
+	return rows, nil
 }
