07e85e64d943a6f209c9f0586431d9c1d76c38f2
matthung0807 <bronx0807@gmail.com>
2022-05-19 16:13:06 +0800

    go panic and recover

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # go-demo
-Go Demo
+[Golang panic recover簡介](https://matthung0807.blogspot.com/2022/05/go-panic-recover.html)
--- main.go
+++ main.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("recovered", r)
+		}
+	}()
+	doPanic()
+	fmt.Println("hello world")
+}
+
+func doPanic() {
+	fmt.Println("before panic")
+	defer fmt.Println("defer after panic")
+
+	panic("panic!!")
 }
