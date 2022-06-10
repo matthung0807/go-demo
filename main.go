9dc7313bd3e42ad40f552c42021233794755983c
matthung0807 <bronx0807@gmail.com>
2022-06-10 17:34:05 +0800

    upload file

--- main.go
+++ main.go
@@ -1,15 +1,38 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"net/http"
+	"os"
 )
 
 func main() {
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		name := r.URL.Query().Get("name") // get URL query string
-		content := fmt.Sprintf("hello, %s", name)
-		fmt.Fprint(w, content) // write out content
+	http.HandleFunc("/upload", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodPost:
+			file, header, err := r.FormFile("file") // get form file input
+			if err != nil {
+				panic(err)
+			}
+			defer file.Close()
+
+			buf := bytes.NewBuffer(nil)
+			_, err = io.Copy(buf, file)
+			if err != nil {
+				panic(err)
+			}
+			err = os.WriteFile(header.Filename, buf.Bytes(), 0666)
+			if err != nil {
+				panic(err)
+			}
+
+			fmt.Fprint(w, fmt.Sprintf("%v uploaded", header.Filename))
+
+		default:
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		}
 	})
 
 	http.ListenAndServe(":8080", nil)
