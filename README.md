be34f422bdd73686b1de767aa2a4f18404e130a1
matthung0807 <bronx0807@gmail.com>
2022-06-10 22:43:26 +0800

    receive form data

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # go-demo
-[Golang 上傳檔案到 HTTP Web Server](https://matthung0807.blogspot.com/2022/06/go-upload-file-to-http-web-server.html)
+[Golang Web接收表單資料](https://matthung0807.blogspot.com/2022/06/go-web-receive-form-data.html)
--- main.go
+++ main.go
@@ -1,35 +1,25 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"io"
 	"net/http"
-	"os"
 )
 
 func main() {
-	http.HandleFunc("/upload", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/employee", func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case http.MethodPost:
-			file, header, err := r.FormFile("file") // get form file input
-			if err != nil {
-				panic(err)
-			}
-			defer file.Close()
+			r.ParseForm()
+			name := r.Form.Get("name")
+			email := r.Form.Get("email")
+			age := r.Form.Get("age")
+			birthday := r.Form.Get("birthday")
+			gender := r.Form.Get("gender")
+			langs := r.Form["lang"]
 
-			buf := bytes.NewBuffer(nil)
-			_, err = io.Copy(buf, file)
-			if err != nil {
-				panic(err)
-			}
-			err = os.WriteFile(header.Filename, buf.Bytes(), 0666)
-			if err != nil {
-				panic(err)
-			}
-
-			fmt.Fprint(w, fmt.Sprintf("%v uploaded", header.Filename))
-
+			fmt.Fprint(w, fmt.Sprintf(
+				"name=%v\nemail=%v\nage=%v\nbirtyday=%v\ngender=%v\nlanguages=%v",
+				name, email, age, birthday, gender, langs))
 		default:
 			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		}
