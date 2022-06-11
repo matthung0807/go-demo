6e815d49ff6b71fe17fc60c1ac67773cbe415756
matthung0807 <bronx0807@gmail.com>
2022-06-11 22:38:29 +0800

    response image

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # go-demo
-[Golang 上傳檔案到 HTTP Web Server](https://matthung0807.blogspot.com/2022/06/go-upload-file-to-http-web-server.html)
+[Golang Web API 回應圖片檔](https://matthung0807.blogspot.com/2022/06/go-web-api-response-image.html)
--- main.go
+++ main.go
@@ -1,39 +1,31 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
-	"io"
 	"net/http"
-	"os"
 )
 
 func main() {
-	http.HandleFunc("/upload", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/gopher", func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
-		case http.MethodPost:
-			file, header, err := r.FormFile("file") // get form file input
-			if err != nil {
-				panic(err)
-			}
-			defer file.Close()
+		case http.MethodGet:
+			/* 範例一 */
+			// b, err := ioutil.ReadFile("./static/image/gopher.jpg")
+			// if err != nil {
+			// 	panic(err)
+			// }
+			// w.Header().Set("Content-Type", "image/jpeg")
+			// w.Write(b)
 
-			buf := bytes.NewBuffer(nil)
-			_, err = io.Copy(buf, file)
-			if err != nil {
-				panic(err)
-			}
-			err = os.WriteFile(header.Filename, buf.Bytes(), 0666)
-			if err != nil {
-				panic(err)
-			}
-
-			fmt.Fprint(w, fmt.Sprintf("%v uploaded", header.Filename))
-
+			/* 範例二 */
+			http.ServeFile(w, r, "./static/image/gopher.jpg")
 		default:
 			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		}
 	})
+
+	/* 範例三 */
+	fs := http.FileServer(http.Dir("./static/image"))
+	http.Handle("/image/", http.StripPrefix("/image/", fs))
 
 	http.ListenAndServe(":8080", nil)
 }
