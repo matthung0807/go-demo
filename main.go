fbd24f6c7ac62fc9f28d169e905dd8a6354b633a
Leo Hung <leo.hung@idcservices.net>
2022-11-18 12:05:32 +0800

    go rabbitmq hello world

--- main.go
+++ main.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	// 取得連線
+	conn, err := amqp.Dial("amqp://guest:guest@mq01.mqloud.dev:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	// 建立通道
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	// 建立queue
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	// 設定回應時間
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	body := "hello world" // 訊息內容
+	err = ch.PublishWithContext(ctx,
+		"",     // default exchange
+		q.Name, // routing key
+		true,   // mandatory
+		false,  // immediate
+		// 設定訊息屬性
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
+	log.Printf("message sent\n")
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
 }
