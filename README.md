e7619ab6b9cbfbe325cee752486bd67a56234808
Leo Hung <leo.hung@idcservices.net>
2022-11-18 14:52:06 +0800

    go rabbitmq hello world send

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # go-demo
-Go Demo
+[Golang RabbitMQ hello world 發送訊息範例](https://matthung0807.blogspot.com/2022/11/go-rabbitmq-hello-world.html)
--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	// 取得連線
-	conn, err := amqp.Dial("amqp://guest:guest@mq01.mqloud.dev:5672/")
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
