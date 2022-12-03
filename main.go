7cdee9da9f0db993228a0bfaf42b8a7ca5a1843f
matthung0807 <bronx0807@gmail.com>
2022-12-03 14:59:26 +0800

    websocket hello world

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true // allow CORS request's Origin header
+	},
+}
 
 func main() {
-	fmt.Println("Hello World")
+	http.HandleFunc("/echo", echoHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func echoHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil) // get a websocket connettion
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+	for {
+		mt, message, err := conn.ReadMessage() // read a amessage from client
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("receive: %s", message)
+		err = conn.WriteMessage(mt, message) // write a message to client
+		if err != nil {
+			panic(err)
+		}
+	}
 }
