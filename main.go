0c1ed0ede81b9f7253a090f886763b68e641067f
Leo Hung <leo.hung@idcservices.net>
2023-01-06 17:06:12 +0800

    fix

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	r := mq.NewRabbitMQ("amqp://guest:guest@mq01.mqloud.dev:5672/")
+	r := mq.NewRabbitMQ("amqp://guest:guest@localhost:5672/")
 	r.Connect(time.Second * 5)
 	r.Consume()
 }
