dc715a9708ceac4a053310f040638fef8c76c470
matthung0807 <bronx0807@gmail.com>
2023-01-21 12:15:52 +0800

    go colly scraper

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # go-demo
-Go Demo
+[Golang 網頁爬蟲範例](https://matthung0807.blogspot.com/2023/01/go-colly-web-scraper.html)
--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gocolly/colly/v2"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	c := colly.NewCollector()
+
+	c.OnHTML("h1.title", func(e *colly.HTMLElement) {
+		s := e.Text // the text of &lt;h1 class="title"/&gt;
+		fmt.Println(s)
+	})
+
+	c.Visit("https://matthung0807.blogspot.com/")
 }
