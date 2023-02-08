3fb0bb075c3310b05b8323a592c7317f55571100
Leo Hung <leo.hung@idcservices.net>
2023-02-08 19:13:18 +0800

    delete direct connect virtual interface

--- main.go
+++ main.go
@@ -12,27 +12,17 @@ func main() {
 	ctx := context.TODO()
 	client := NewDirectConnectClient(ctx)
 
-	connectionId := "dxcon-fg5kq63s"
-	params := &directconnect.DescribeVirtualInterfacesInput{
-		ConnectionId: &connectionId,
+	virtualInterfaceId := "dxvif-ffwha4ij"
+	params := &directconnect.DeleteVirtualInterfaceInput{
+		VirtualInterfaceId: &virtualInterfaceId,
 	}
 
-	output, err := client.DescribeVirtualInterfaces(ctx, params)
+	output, err := client.DeleteVirtualInterface(ctx, params)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(*output.VirtualInterfaces[0].VirtualInterfaceId)          // dxvif-ffwha4ij
-	fmt.Println(*output.VirtualInterfaces[0].VirtualInterfaceName)        // demo-virtual-interface-001
-	fmt.Println(output.VirtualInterfaces[0].VirtualInterfaceState)        // down
-	fmt.Println(*output.VirtualInterfaces[0].VirtualInterfaceType)        // private
-	fmt.Println(*output.VirtualInterfaces[0].DirectConnectGatewayId)      // e44e0dfb-82b9-4e4f-bcc1-9d196f25d0af
-	fmt.Println(*output.VirtualInterfaces[0].AmazonSideAsn)               // 64512
-	fmt.Println(*output.VirtualInterfaces[0].BgpPeers[0].BgpPeerId)       // dxpeer-fgzqetz9
-	fmt.Println(output.VirtualInterfaces[0].BgpPeers[0].BgpPeerState)     // available
-	fmt.Println(output.VirtualInterfaces[0].BgpPeers[0].BgpStatus)        // down
-	fmt.Println(*output.VirtualInterfaces[0].BgpPeers[0].AmazonAddress)   // 169.254.96.1/29
-	fmt.Println(*output.VirtualInterfaces[0].BgpPeers[0].CustomerAddress) // 169.254.96.6/29
+	fmt.Println(*&output.VirtualInterfaceState) // deleting
 }
 
 func NewDirectConnectClient(ctx context.Context) *directconnect.Client {
