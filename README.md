f863de494403aad01fae7b58fc97e0bd921d7357
Leo Hung <leo.hung@idcservices.net>
2023-02-08 18:59:26 +0800

    get direct connect virtual interface

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # go-demo
-[Golang 建立AWS Direct Connect Private Virtual Interface](https://matthung0807.blogspot.com/2023/02/go-create-aws-direct-connect-private-virtual-interface.html)
+[Golang 取得AWS Direct Connect Virtual Interface](https://matthung0807.blogspot.com/2023/02/go-get-aws-direct-connect-private-virtual-interface.html)
--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/directconnect"
-	"github.com/aws/aws-sdk-go-v2/service/directconnect/types"
 )
 
 func main() {
@@ -14,41 +13,26 @@ func main() {
 	client := NewDirectConnectClient(ctx)
 
 	connectionId := "dxcon-fg5kq63s"
-
-	virtualInterfaceName := "demo-virtual-interface-001"
-	authKey := "bgp-auth-key"
-	directConnectGatewayId := "e44e0dfb-82b9-4e4f-bcc1-9d196f25d0af"
-	newPrivateVirtualInterface := types.NewPrivateVirtualInterface{
-		Asn:                    4567,
-		VirtualInterfaceName:   &virtualInterfaceName,
-		Vlan:                   123,
-		AddressFamily:          types.AddressFamilyIPv4,
-		AuthKey:                &authKey,
-		DirectConnectGatewayId: &directConnectGatewayId,
+	params := &directconnect.DescribeVirtualInterfacesInput{
+		ConnectionId: &connectionId,
 	}
 
-	params := &directconnect.CreatePrivateVirtualInterfaceInput{
-		ConnectionId:               &connectionId,
-		NewPrivateVirtualInterface: &newPrivateVirtualInterface,
-	}
-
-	output, err := client.CreatePrivateVirtualInterface(ctx, params)
+	output, err := client.DescribeVirtualInterfaces(ctx, params)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(*output.VirtualInterfaceId)          // dxvif-ffwha4ij
-	fmt.Println(*output.VirtualInterfaceName)        // demo-virtual-interface-001
-	fmt.Println(output.VirtualInterfaceState)        // pending
-	fmt.Println(*output.VirtualInterfaceType)        // private
-	fmt.Println(*output.ConnectionId)                // dxcon-fg5kq63s
-	fmt.Println(*output.DirectConnectGatewayId)      // e44e0dfb-82b9-4e4f-bcc1-9d196f25d0af
-	fmt.Println(*output.AmazonSideAsn)               // 64512
-	fmt.Println(*output.BgpPeers[0].BgpPeerId)       // dxpeer-fgzqetz9
-	fmt.Println(output.BgpPeers[0].BgpPeerState)     // pending
-	fmt.Println(output.BgpPeers[0].BgpStatus)        // down
-	fmt.Println(*output.BgpPeers[0].AmazonAddress)   // 169.254.96.1/29
-	fmt.Println(*output.BgpPeers[0].CustomerAddress) // 169.254.96.6/29
+	fmt.Println(*output.VirtualInterfaces[0].VirtualInterfaceId)          // dxvif-ffwha4ij
+	fmt.Println(*output.VirtualInterfaces[0].VirtualInterfaceName)        // demo-virtual-interface-001
+	fmt.Println(output.VirtualInterfaces[0].VirtualInterfaceState)        // down
+	fmt.Println(*output.VirtualInterfaces[0].VirtualInterfaceType)        // private
+	fmt.Println(*output.VirtualInterfaces[0].DirectConnectGatewayId)      // e44e0dfb-82b9-4e4f-bcc1-9d196f25d0af
+	fmt.Println(*output.VirtualInterfaces[0].AmazonSideAsn)               // 64512
+	fmt.Println(*output.VirtualInterfaces[0].BgpPeers[0].BgpPeerId)       // dxpeer-fgzqetz9
+	fmt.Println(output.VirtualInterfaces[0].BgpPeers[0].BgpPeerState)     // available
+	fmt.Println(output.VirtualInterfaces[0].BgpPeers[0].BgpStatus)        // down
+	fmt.Println(*output.VirtualInterfaces[0].BgpPeers[0].AmazonAddress)   // 169.254.96.1/29
+	fmt.Println(*output.VirtualInterfaces[0].BgpPeers[0].CustomerAddress) // 169.254.96.6/29
 }
 
 func NewDirectConnectClient(ctx context.Context) *directconnect.Client {
