4973d5a2f97d36265cfc2b9fa758e76163cbfceb
Leo Hung <leo.hung@idcservices.net>
2023-02-08 11:46:31 +0800

    create virtual private gateway

--- main.go
+++ main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	ctx := context.TODO()
+	client := NewEC2Client(ctx)
+
+	amazonSideAsn := int64(64512)
+	key := "Name"
+	value := "demo-virtual-private-gateway-001"
+	tag := types.Tag{
+		Key:   &key,
+		Value: &value,
+	}
+	tagSpecification := types.TagSpecification{
+		ResourceType: types.ResourceTypeVpnGateway,
+		Tags:         []types.Tag{tag},
+	}
+	params := &ec2.CreateVpnGatewayInput{
+		Type:              types.GatewayTypeIpsec1,
+		AmazonSideAsn:     &amazonSideAsn,
+		TagSpecifications: []types.TagSpecification{tagSpecification},
+	}
+
+	output, err := client.CreateVpnGateway(ctx, params)
+	if err != nil {
+		panic(err)
+	}
+
+	vpg := output.VpnGateway
+	fmt.Println(*vpg.VpnGatewayId) // vgw-0670c529abefaee33
+}
+
+func NewEC2Client(ctx context.Context) *ec2.Client {
+	cfg, err := config.LoadDefaultConfig(
+		ctx,
+		config.WithRegion("ap-northeast-1"),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	return ec2.NewFromConfig(cfg) // Create an Amazon EC2 service client
 }
