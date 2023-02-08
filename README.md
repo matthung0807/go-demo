8815c81fcc06e91095c49a76dd323f402071b457
Leo Hung <leo.hung@idcservices.net>
2023-02-08 13:31:09 +0800

    attach virtual private gateway to vpc

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # go-demo
-[Golang 建立AWS VPC Virtual private gateway](https://matthung0807.blogspot.com/2023/02/go-create-aws-vpc-virtual-private-gateway.html)
+[Golang AWS VPC 附加Virtual private gateway](https://matthung0807.blogspot.com/2023/02/go-aws-vpc-attach-virutal-private-gateway.html)
--- main.go
+++ main.go
@@ -7,37 +7,25 @@ import (
 	"github.com/aws/aws-sdk-go-v2/config"
 
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
-	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
 )
 
 func main() {
 	ctx := context.TODO()
 	client := NewEC2Client(ctx)
 
-	amazonSideAsn := int64(64512)
-	key := "Name"
-	value := "demo-virtual-private-gateway-001"
-	tag := types.Tag{
-		Key:   &key,
-		Value: &value,
+	vpcId := "vpc-0e6e56e06a48ef314"
+	vpnGatewayId := "vgw-0670c529abefaee33"
+	params := &ec2.AttachVpnGatewayInput{
+		VpcId:        &vpcId,
+		VpnGatewayId: &vpnGatewayId,
 	}
-	tagSpecification := types.TagSpecification{
-		ResourceType: types.ResourceTypeVpnGateway,
-		Tags:         []types.Tag{tag},
-	}
-	params := &ec2.CreateVpnGatewayInput{
-		Type:              types.GatewayTypeIpsec1,
-		AmazonSideAsn:     &amazonSideAsn,
-		TagSpecifications: []types.TagSpecification{tagSpecification},
-	}
-
-	output, err := client.CreateVpnGateway(ctx, params)
+	output, err := client.AttachVpnGateway(ctx, params)
 	if err != nil {
 		panic(err)
 	}
 
-	vpg := output.VpnGateway
-	fmt.Println(*vpg.VpnGatewayId) // vgw-0670c529abefaee33
+	fmt.Println(*output.VpcAttachment.VpcId) // vpc-0e6e56e06a48ef314
+	fmt.Println(output.VpcAttachment.State)  // attaching
 }
 
 func NewEC2Client(ctx context.Context) *ec2.Client {
