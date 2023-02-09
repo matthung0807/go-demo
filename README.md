2bc379c503b6905525e6c328c6de2ac74c56989c
Leo Hung <leo.hung@idcservices.net>
2023-02-09 10:00:07 +0800

    delete direct connect gateway

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # go-demo
-[Golang 建立AWS Direct Connect gateway](https://matthung0807.blogspot.com/2023/02/go-create-aws-direct-connect-gateway.html)
+[Golang 刪除AWS Direct Connect gateway](https://matthung0807.blogspot.com/2023/02/go-delete-aws-direct-connect-gateway.html)
--- main.go
+++ main.go
@@ -12,22 +12,18 @@ func main() {
 	ctx := context.TODO()
 	client := NewDirectConnectClient(ctx)
 
-	directConnectGatewayName := "demo-directconnect-gateway-001"
-	amazonSideAsn := int64(64512)
-	params := &directconnect.CreateDirectConnectGatewayInput{
-		DirectConnectGatewayName: &directConnectGatewayName,
-		AmazonSideAsn:            &amazonSideAsn,
+	directConnectGatewayId := "e44e0dfb-82b9-4e4f-bcc1-9d196f25d0af"
+	params := &directconnect.DeleteDirectConnectGatewayInput{
+		DirectConnectGatewayId: &directConnectGatewayId,
 	}
 
-	output, err := client.CreateDirectConnectGateway(ctx, params)
+	output, err := client.DeleteDirectConnectGateway(ctx, params)
 	if err != nil {
 		panic(err)
 	}
 
 	dcg := output.DirectConnectGateway
-
-	fmt.Println(*dcg.DirectConnectGatewayId)   // e44e0dfb-82b9-4e4f-bcc1-9d196f25d0af
-	fmt.Println(dcg.DirectConnectGatewayState) // available
+	fmt.Println(dcg.DirectConnectGatewayState) // deleting
 }
 
 func NewDirectConnectClient(ctx context.Context) *directconnect.Client {
