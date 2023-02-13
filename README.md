63bd677ffcbb18df6a8220d72129b18fbb10e28d
Leo Hung <leo.hung@idcservices.net>
2023-02-13 13:30:38 +0800

    create gcp interconnect cloud router

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # go-demo
-Go Demo
+[Golang 建立GCP Interconnect Cloud Router](https://matthung0807.blogspot.com/2023/02/go-create-gcp-interconnect-cloud-router.html)
--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"context"
+
+	compute "google.golang.org/api/compute/v1"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	ctx := context.Background()
+	service, err := compute.NewService(ctx)
+	if err != nil {
+		panic(err)
+	}
+	routersService := compute.NewRoutersService(service)
+
+	projectId := "project-id-1"
+	region := "asia-east1"
+	router := &compute.Router{
+		Name:    "demo-cloudrouter-002",
+		Network: "projects/project-id-1/global/networks/demo-vpc-001",
+		Bgp: &compute.RouterBgp{
+			AdvertiseMode: "DEFAULT",
+			Asn:           int64(16550),
+		},
+	}
+
+	call := routersService.Insert(projectId, region, router)
+	_, err = call.Do()
+	if err != nil {
+		panic(err)
+	}
 }
