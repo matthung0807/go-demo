971c31ea7c4c6fbe2c043afb80e54fb347b51683
Leo Hung <leo.hung@idcservices.net>
2023-02-13 13:51:14 +0800

    get gcp interconnect cloud router

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # go-demo
-[Golang 建立GCP Interconnect Cloud Router](https://matthung0807.blogspot.com/2023/02/go-create-gcp-interconnect-cloud-router.html)
+[Golang 取得GCP Interconnect Cloud Router](https://matthung0807.blogspot.com/2023/02/go-get-gcp-interconnect-cloud-router.html)
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	compute "google.golang.org/api/compute/v1"
 )
@@ -14,20 +15,19 @@ func main() {
 	}
 	routersService := compute.NewRoutersService(service)
 
-	projectId := "project-id-1"
+	projectId := "allen-test-312507"
 	region := "asia-east1"
-	router := &compute.Router{
-		Name:    "demo-cloudrouter-002",
-		Network: "projects/project-id-1/global/networks/demo-vpc-001",
-		Bgp: &compute.RouterBgp{
-			AdvertiseMode: "DEFAULT",
-			Asn:           int64(16550),
-		},
-	}
-
-	call := routersService.Insert(projectId, region, router)
-	_, err = call.Do()
+	routerName := "demo-cloudrouter-002"
+	call := routersService.Get(projectId, region, routerName)
+	router, err := call.Do()
 	if err != nil {
 		panic(err)
 	}
+
+	fmt.Println(router.Name)              // demo-cloudrouter-002
+	fmt.Println(router.Kind)              // compute#router
+	fmt.Println(router.Bgp.Asn)           // 16550
+	fmt.Println(router.Bgp.AdvertiseMode) // DEFAULT
+	fmt.Println(router.Network)           // https://www.googleapis.com/compute/v1/projects/project-id-1/global/networks/demo-vpc-001
+	fmt.Println(router.SelfLink)          // https://www.googleapis.com/compute/v1/projects/project-id-1/regions/asia-east1/routers/demo-cloudrouter-002
 }
