ea05aea4f5f918c45c8ae37c9e520282f4dd74da
Leo Hung <leo.hung@idcservices.net>
2023-03-21 18:46:35 +0800

    create parnter interconnect vlan attachment with cloudrouter to vpc

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # go-demo
-[Golang 取得GCP Interconnect Cloud Router](https://matthung0807.blogspot.com/2023/02/go-get-gcp-interconnect-cloud-router.html)
+[Golang 建立GCP Partner Interconnect connection VLAN Attachment, CloudRouter to VPC network](https://matthung0807.blogspot.com/2023/03/go-create-gcp-partner-interconnect-vlan-attachment-cloudrouter-to-vpc.html)
--- main.go
+++ main.go
@@ -13,18 +13,48 @@ func main() {
 		panic(err)
 	}
 
+	routersService := compute.NewRoutersService(service)
+
 	projectId := "project-id-1"
 	region := "asia-east1"
+
+	routerName := "demo-cloudrouter-002"
+	router := &compute.Router{
+		Name:    routerName,
+		Network: "projects/project-id-1/global/networks/demo-vpc-001",
+		Bgp: &compute.RouterBgp{
+			AdvertiseMode: "DEFAULT",
+			Asn:           int64(16550),
+		},
+	}
+
+	op, err := routersService.Insert(projectId, region, router).Do()
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = service.RegionOperations.Wait(projectId, region, op.Name).Do()
+	if err != nil {
+		panic(err)
+	}
+
+	router, err = service.Routers.Get(projectId, region, routerName).Do()
+	if err != nil {
+		panic(err)
+	}
 
 	interconnectAttachmentService := compute.NewInterconnectAttachmentsService(service)
 	vlanAttachement := &compute.InterconnectAttachment{
 		Name:                   "demo-vlan-b-002",
 		EdgeAvailabilityDomain: "AVAILABILITY_DOMAIN_1",
-		Router:                 "https://www.googleapis.com/compute/v1/projects/project-id-1/regions/asia-east1/routers/demo-cloudrouter-002",
+		Router:                 router.SelfLink,
 		Type:                   "PARTNER",
 	}
-	call := interconnectAttachmentService.Insert(projectId, region, vlanAttachement)
-	_, err = call.Do()
+	op, err = interconnectAttachmentService.Insert(projectId, region, vlanAttachement).Do()
+	if err != nil {
+		panic(err)
+	}
+	_, err = service.RegionOperations.Wait(projectId, region, op.Name).Do()
 	if err != nil {
 		panic(err)
 	}
