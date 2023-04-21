74bf14849fc5fddc83734f8b16806b5360497a0c
Leo Hung <leo.hung@idcservices.net>
2023-04-21 14:46:18 +0800

    delete gcp vpc network private service connection allocated ip range

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # go-demo
-[Golang 取得GCP VPC network private service connection IP allocated range清單](https://matthung0807.blogspot.com/2023/04/go-list-vpc-network-private-service-connection-ip-range.html)
+[Golang 刪除GCP VPC network private service connection IP allocated range](https://matthung0807.blogspot.com/2023/04/go-delete-vpc-network-private-service-connection-ip-range.html)
--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"strings"
 
 	compute "google.golang.org/api/compute/v1"
 )
@@ -18,20 +16,12 @@ func main() {
 	globalAddressesService := compute.NewGlobalAddressesService(service)
 
 	projectId := "project-id-1"
-	call := globalAddressesService.List(projectId)
+	addressResourceName := "demo-vpc-002-allocated-range-001"
+	call := globalAddressesService.Delete(projectId, addressResourceName)
 
-	addressList, err := call.Do()
+	_, err = call.Do()
 	if err != nil {
 		panic(err)
-	}
-
-	vpcSelfLink := "projects/proejct-id-1/global/networks/demo-vpc-002"
-	for _, item := range addressList.Items {
-		if strings.Contains(item.Network, vpcSelfLink) {
-			fmt.Println(item.Network)
-			fmt.Println(item.Name)
-			fmt.Printf("%s/%d\n", item.Address, item.PrefixLength)
-		}
 	}
 
 }
