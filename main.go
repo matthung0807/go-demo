0b34e1140823aadacc65195ed28edd7b69cadf65
Leo Hung <leo.hung@idcservices.net>
2023-05-12 12:42:15 +0800

    get aws s3 bucket access point

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3control"
-	"github.com/aws/aws-sdk-go-v2/service/s3control/types"
 )
 
 func main() {
@@ -15,27 +14,23 @@ func main() {
 	client := NewS3ControlClient(ctx)
 
 	accountId := "423456789012"
-	bucket := "aws-s3-bucket-202305021730" // bucket name
-	apName := "ap-1"                       // access point name
+	apName := "ap-1" // access point name
 
-	input := &s3control.CreateAccessPointInput{
+	input := &s3control.GetAccessPointInput{
 		AccountId: &accountId,
-		Bucket:    &bucket,
 		Name:      &apName,
-		PublicAccessBlockConfiguration: &types.PublicAccessBlockConfiguration{
-			BlockPublicAcls:       false,
-			BlockPublicPolicy:     false,
-			IgnorePublicAcls:      false,
-			RestrictPublicBuckets: false,
-		},
 	}
-	output, err := client.CreateAccessPoint(ctx, input)
+
+	output, err := client.GetAccessPoint(ctx, input)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(*output.AccessPointArn) // arn:aws:s3:ap-northeast-1:423456789012:accesspoint/ap-1
-	fmt.Println(*output.Alias)          // ap-1-fpebhno1smg31ehcy4heps8dkz664apn1a-s3alias
+	fmt.Println(*output.AccessPointArn)    // arn:aws:s3:ap-northeast-1:423456789012:accesspoint/ap-1
+	fmt.Println(*output.Alias)             // ap-1-fpebhno1smg31ehcy4heps8dkz664apn1a-s3alias
+	fmt.Println(*output.BucketAccountId)   // 423456789012
+	fmt.Println(*&output.NetworkOrigin)    // Internet
+	fmt.Println(*&output.VpcConfiguration) // nil
 
 }
 
