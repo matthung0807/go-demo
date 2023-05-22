c81061e7e3fe8532be3d71fd20e9d9592b712d64
Leo Hung <leo.hung@idcservices.net>
2023-05-22 18:45:24 +0800

    simple

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+
+	"github.com/go-co-op/gocron"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	sch := gocron.NewScheduler(time.UTC) // create a new job scheduler
+	count := 0
+	_, err := sch.Every(1).Seconds().Do(func() { // create a job to run function every 1 second
+		count++
+		fmt.Println(count)
+	})
+	if err != nil {
+		panic(err)
+	}
+	sch.StartAsync()            // start the scheduler's jobs in another goroutine
+	time.Sleep(time.Second * 5) // main goroutine sleep for 5 second to let scheduler goroutine have time run
 }
