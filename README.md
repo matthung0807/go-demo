998292025de492ba0cb07df1fdac2b3f8642bf27
Leo Hung <leo.hung@idcservices.net>
2023-05-23 16:49:03 +0800

    create aws ec2 instance

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # go-demo
-[Golang 建立AWS VPC](https://matthung0807.blogspot.com/2023/03/go-create-aws-vpc.html)
+[Golang 建立AWS EC2 instance](https://matthung0807.blogspot.com/2023/05/go-create-aws-ec2-instance.html)
--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
 	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
@@ -13,30 +15,65 @@ func main() {
 	ctx := context.TODO()
 	client := NewEC2Client(ctx)
 
-	key := "Name"
-	value := "demo-vpc-002"
+	instanceName := "demo-ec2-instance-002"
+	imageId := "ami-0e0820ad173f20fbb" // Amazon Linux 2023 AMI 2023.0.20230503.0 x86_64 HVM kernel-6.1
+	KeyName := "demo-key"              // key pairs name
+	securityGroupId := "sg-0e44d58befeb89923"
+	subnetId := "subnet-05c7f25587be4dc58"
 
-	cidrBlock := "10.1.0.0/24"
-	input := &ec2.CreateVpcInput{
-		CidrBlock: &cidrBlock,
+	input := &ec2.RunInstancesInput{
+		MaxCount:       aws.Int32(1),
+		MinCount:       aws.Int32(1),
+		DisableApiStop: aws.Bool(false),
+		ImageId:        &imageId,
+		InstanceType:   types.InstanceTypeT2Micro, // t2.micro
+		KeyName:        &KeyName,
+		NetworkInterfaces: []types.InstanceNetworkInterfaceSpecification{
+			{
+				AssociatePublicIpAddress: aws.Bool(true),
+				DeleteOnTermination:      aws.Bool(true),
+				DeviceIndex:              aws.Int32(0),
+				Groups: []string{
+					securityGroupId,
+				},
+				InterfaceType:    aws.String("interface"),
+				NetworkCardIndex: aws.Int32(0),
+				SubnetId:         &subnetId,
+			},
+		},
 		TagSpecifications: []types.TagSpecification{
 			{
-				ResourceType: types.ResourceTypeVpc,
+				ResourceType: types.ResourceTypeInstance,
 				Tags: []types.Tag{
 					{
-						Key:   &key,
-						Value: &value,
+						Key:   aws.String("Name"),
+						Value: aws.String(instanceName),
 					},
 				},
 			},
 		},
 	}
-	output, err := client.CreateVpc(ctx, input)
+	output, err := client.RunInstances(ctx, input)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(*output.Vpc.VpcId)     // vpc-019a7b633eda5caae
-	fmt.Println(*output.Vpc.CidrBlock) // 10.1.0.0/24
+
+	waiter := ec2.NewInstanceRunningWaiter(client)
+	describeInput := &ec2.DescribeInstancesInput{
+		InstanceIds: []string{aws.ToString(output.Instances[0].InstanceId)},
+	}
+	maxWaitDur := 60 * time.Second
+	describeOutput, err := waiter.WaitForOutput(ctx, describeInput, maxWaitDur)
+	if err != nil {
+		panic(err)
+	}
+
+	instance := describeOutput.Reservations[0].Instances[0]
+	fmt.Println(*instance.InstanceId)                              // i-0dbcc73ace6b52d42
+	fmt.Println(*instance.SubnetId)                                // subnet-05c7f25587be4dc58
+	fmt.Println(*instance.NetworkInterfaces[0].NetworkInterfaceId) // eni-0cd6cc75c832083f3
+	fmt.Println(*instance.PublicIpAddress)                         // 3.112.238.189
+	fmt.Println(*instance.PrivateIpAddress)                        // 10.1.0.53
 }
 
 func NewEC2Client(ctx context.Context) *ec2.Client {
