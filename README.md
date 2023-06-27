015ee60f50388335c1f16c248c14386421d403e5
Leo Hung <leo.hung@idcservices.net>
2023-06-27 14:19:43 +0800

    get gcp project info

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # go-demo
-[Golang 取得GCP Compute VM instance](https://matthung0807.blogspot.com/2023/02/go-get-gcp-compute-vm-instance.html)
+[Golang 取得GCP Project資訊](https://matthung0807.blogspot.com/2023/06/go-get-gcp-project-info.html)
--- main.go
+++ main.go
@@ -4,32 +4,22 @@ import (
 	"context"
 	"fmt"
 
-	compute "google.golang.org/api/compute/v1"
+	"google.golang.org/api/cloudresourcemanager/v1"
 )
 
 func main() {
 	ctx := context.Background()
-	service, err := compute.NewService(ctx)
-	if err != nil {
-		panic(err)
-	}
-	instancesService := compute.NewInstancesService(service)
-
-	projectId := "project-id-1"
-	zone := "asia-east1-b"
-	instanceName := "demo-instance"
-	call := instancesService.Get(projectId, zone, instanceName)
-
-	instance, err := call.Do()
+	crmService, err := cloudresourcemanager.NewService(ctx)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(instance.Id)                    // 6192712587621936341
-	fmt.Println(instance.Name)                  // demo-instance
-	fmt.Println(instance.MachineType)           // https://www.googleapis.com/compute/beta/projects/project-id-1/zones/asia-east1-b/machineTypes/e2-micro
-	fmt.Println(instance.Disks[0].DiskSizeGb)   // 20
-	fmt.Println(instance.Disks[0].DeviceName)   // demo-instance
-	fmt.Println(instance.Disks[0].Architecture) // X86_64
-	fmt.Println(instance.Disks[0].Boot)         // true
+	projectId := "allen-test-312507"
+	project, err := crmService.Projects.Get(projectId).Do()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(project.Name)          // gcp-demo
+	fmt.Println(project.ProjectId)     // project-id-1
+	fmt.Println(project.ProjectNumber) // 948024786833
 }
