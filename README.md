2181098d2ec11d7804269e2242791ad5426cd4dd
Leo Hung <leo.hung@idcservices.net>
2023-10-11 18:25:38 +0800

    go template hello world

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # go-demo
-Go Demo
+[Golang template 模板簡單範例](https://matthung0807.blogspot.com/2023/10/go-template-example.html)
--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"os"
+	"text/template"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	text := "hello {{.}}"                                // 模板內容。{{.}}會插入資料物件
+	t := template.Must(template.New("demo").Parse(text)) // 解析模板內容來建立名稱為'demo'的模板
+
+	data := "john"
+	err := t.Execute(os.Stdout, data) // 將資料物件套用在模板，並將結果輸出到標準輸出
+	if err != nil {
+		panic(err)
+	}
 }
