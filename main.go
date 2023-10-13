54d3c3a9b46fa4a68466cd70618c24616384b9eb
Leo Hung <leo.hung@idcservices.net>
2023-10-13 13:50:56 +0800

    go template range

--- main.go
+++ main.go
@@ -5,13 +5,24 @@ import (
 	"text/template"
 )
 
+type Employee struct {
+	Name string
+	Age  int
+}
+
 func main() {
-	text := "hello {{.}}"                                // 模板內容。{{.}}會插入資料物件
+	text := `Employees:
+{{range .}}    Name:{{.Name}}, Age:{{.Age}}
+{{end}}`
 	t := template.Must(template.New("demo").Parse(text)) // 解析模板內容來建立名稱為'demo'的模板
 
-	data := "john"
+	data := []Employee{
+		{"John", 33},
+		{"Mary", 28},
+	}
 	err := t.Execute(os.Stdout, data) // 將資料物件套用在模板，並將結果輸出到標準輸出
 	if err != nil {
 		panic(err)
 	}
+
 }
