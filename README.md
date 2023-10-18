0f0fe085c4556509e3a93693175781975ef91615
Leo Hung <leo.hung@idcservices.net>
2023-10-18 16:29:31 +0800

    go template range html table rows

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # go-demo
-[Golang template range用法](https://matthung0807.blogspot.com/2023/10/blog-post_11.html)
+[Golang template range html table rows](https://matthung0807.blogspot.com/2023/10/golang-template-range-html-table-rows.html)
--- main.go
+++ main.go
@@ -1,28 +1,46 @@
 package main
 
 import (
+	"bytes"
+	_ "embed"
+	"html/template"
 	"os"
-	"text/template"
 )
 
+type Table struct {
+	Name      string
+	Employees []Employee
+}
+
 type Employee struct {
+	Id   string
 	Name string
 	Age  int
 }
 
-func main() {
-	text := `Employees:
-{{range .}}    Name:{{.Name}}, Age:{{.Age}}
-{{end}}`
-	t := template.Must(template.New("demo").Parse(text)) // 解析模板內容來建立名稱為'demo'的模板
+//go:embed template
+var htmlTemplate string
 
-	data := []Employee{
-		{"John", 33},
-		{"Mary", 28},
+func main() {
+	t := template.Must(template.New("htmlTemplate").Parse(htmlTemplate))
+
+	employees := []Employee{
+		{"1", "John", 33},
+		{"2", "Mary", 28},
+		{"3", "Tony", 44},
+		{"4", "Bill", 22},
 	}
-	err := t.Execute(os.Stdout, data) // 將資料物件套用在模板，並將結果輸出到標準輸出
+
+	data := Table{
+		Name:      "Employee List",
+		Employees: employees,
+	}
+	buf := new(bytes.Buffer)
+	err := t.Execute(buf, data)
 	if err != nil {
 		panic(err)
 	}
+
+	os.WriteFile("result.html", buf.Bytes(), 0666)
 
 }
