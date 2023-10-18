2823b241aada5939ffed4f3e7947fce443826ac9
Leo Hung <leo.hung@idcservices.net>
2023-10-18 13:24:50 +0800

    functional options pattern

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # go-demo
-Go Demo
+[Golang Functial Options Pattern簡介](https://matthung0807.blogspot.com/2023/10/golang-functional-options-pattern.html)
--- main.go
+++ main.go
@@ -1,7 +1,71 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type Employee struct {
+	Id    string
+	Name  string
+	Email string
+	Phone string
+	Age   int
+}
+
+func (e Employee) String() string {
+	return fmt.Sprintf("{id=%s, name=%s, email=%s, phone=%s, age=%d}",
+		e.Id, e.Name, e.Email, e.Phone, e.Age)
+}
+
+type Option func(e *Employee)
+
+func (option Option) Apply(e *Employee) {
+	option(e)
+}
+
+func NewEmployee(id string, options ...Option) *Employee {
+	e := &Employee{
+		Id: id,
+	}
+	for _, option := range options {
+		option(e)
+	}
+	return e
+}
+
+func WithName(name string) Option {
+	return func(e *Employee) {
+		e.Name = name
+	}
+}
+
+func WithEmail(email string) Option {
+	return func(e *Employee) {
+		e.Email = email
+	}
+}
+
+func WithPhone(phone string) Option {
+	return func(e *Employee) {
+		e.Phone = phone
+	}
+}
+
+func WithAge(age int) Option {
+	return func(e *Employee) {
+		e.Age = age
+	}
+}
 
 func main() {
-	fmt.Println("Hello World")
+	e := NewEmployee(
+		"1",
+		WithName("john"),
+		WithAge(33),
+	)
+	fmt.Println(e)
+
+	WithEmail("john@abc.com").Apply(e)
+	WithPhone("111-222-3333").Apply(e)
+	fmt.Println(e)
 }
