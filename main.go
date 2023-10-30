75cc0c12d90092f2f7c8b9d8482adc810e1d37f8
Leo Hung <leo.hung@idcservices.net>
2023-10-30 18:06:19 +0800

    fx web example

--- main.go
+++ main.go
@@ -1,7 +1,93 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"net"
+	"net/http"
+
+	"go.uber.org/fx"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	fx.New(
+		fx.Provide(
+			NewHTTPServer,
+			fx.Annotate(
+				NewServeMux,
+				fx.ParamTags(`group:"routes"`),
+			),
+			AsRoute(NewEchoHandler),
+			AsRoute(NewHelloHandler),
+		),
+		fx.Invoke(func(*http.Server) {}),
+	).Run()
+}
+
+func AsRoute(f any) any {
+	return fx.Annotate(
+		f,
+		fx.As(new(Route)),
+		fx.ResultTags(`group:"routes"`),
+	)
+}
+
+func NewHTTPServer(lc fx.Lifecycle, mux *http.ServeMux) *http.Server {
+	srv := &http.Server{Addr: ":8080", Handler: mux}
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			ln, err := net.Listen("tcp", srv.Addr)
+			if err != nil {
+				return err
+			}
+			fmt.Println("Starting HTTP server at", srv.Addr)
+			go srv.Serve(ln)
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			return srv.Shutdown(ctx)
+		},
+	})
+	return srv
+}
+
+type Route interface {
+	http.Handler
+	Pattern() string
+}
+
+type EchoHandler struct{}
+
+func NewEchoHandler() *EchoHandler {
+	return &EchoHandler{}
+}
+
+func (h *EchoHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("echo"))
+}
+
+func (h *EchoHandler) Pattern() string {
+	return "/echo"
+}
+
+type HelloHandler struct{}
+
+func NewHelloHandler() *HelloHandler {
+	return &HelloHandler{}
+}
+func (h *HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello"))
+}
+
+func (h *HelloHandler) Pattern() string {
+	return "/hello"
+}
+
+func NewServeMux(routes []Route) *http.ServeMux {
+	mux := http.NewServeMux()
+	for _, route := range routes {
+		mux.Handle(route.Pattern(), route)
+	}
+
+	return mux
 }
