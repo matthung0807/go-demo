ce919cf531ea5ca0ce19687a221cfdfdea40bec2
Leo Hung <leo.hung@idcservices.net>
2023-10-31 16:41:30 +0800

    fx and gin

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module abc.com/demo
 
-go 1.17
+go 1.19
 
 require github.com/gin-gonic/gin v1.7.2
 
@@ -16,7 +16,12 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
 	github.com/ugorji/go/codec v1.1.7 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/dig v1.17.0 // indirect
+	go.uber.org/fx v1.20.1 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.23.0 // indirect
 	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
-	golang.org/x/sys v0.0.0-20200116001909-b77594299b42 // indirect
+	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
 	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
--- main.go
+++ main.go
@@ -1,8 +1,26 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+
+	"github.com/gin-gonic/gin"
+	"go.uber.org/fx"
+)
 
 func main() {
+	fx.New(
+		fx.Provide(
+			AnnotateAsHandler(NewHelloHandler),
+			fx.Annotate(
+				NewRouter,
+				fx.ParamTags(``, `group:"handlers"`),
+			),
+		),
+		fx.Invoke(func(router *Router) {
+			router.Route()
+		}),
+	).Run()
+
 	router := gin.Default()
 	router.GET("/hello", func(c *gin.Context) {
 		c.JSON(200, gin.H{
@@ -10,4 +28,64 @@ func main() {
 		})
 	})
 	router.Run()
+}
+
+func AnnotateAsHandler(f any) any {
+	return fx.Annotate(
+		f,
+		fx.As(new(Hander)),
+		fx.ResultTags(`group:"handler"`),
+	)
+
+}
+
+type Router struct {
+	router   *gin.Engine
+	handlers []Hander
+}
+
+func NewRouter(lc fx.Lifecycle, handlers []Hander) *Router {
+	router := gin.Default()
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			go router.Run()
+			return nil
+		},
+	})
+
+	return &Router{
+		router:   router,
+		handlers: handlers,
+	}
+}
+
+func (r Router) Route() {
+	for _, h := range r.handlers {
+		r.router.Handle(h.HttpMethod(), h.Pattern(), h.HandlerFunc)
+	}
+}
+
+type Hander interface {
+	HandlerFunc(c *gin.Context)
+	HttpMethod() string
+	Pattern() string
+}
+
+type HelloHandler struct{}
+
+func NewHelloHandler() *HelloHandler {
+	return &HelloHandler{}
+}
+func (h *HelloHandler) HandlerFunc(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "hello world",
+	})
+}
+
+func (h *HelloHandler) HttpMethod() string {
+	return "GET"
+}
+
+func (h *HelloHandler) Pattern() string {
+	return "/hello"
 }
