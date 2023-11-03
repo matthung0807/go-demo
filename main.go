b334da94c7a5a6120db2239277c353cd3ceca0bd
Leo Hung <leo.hung@idcservices.net>
2023-11-03 09:48:42 +0800

    uber fx parameter objects

--- main.go
+++ main.go
@@ -6,10 +6,49 @@ import (
 	"go.uber.org/fx"
 )
 
+type S1Param struct {
+	fx.In
+
+	S string `name:"s1"`
+}
+
+type S1Result struct {
+	fx.Out
+
+	S string `name:"s1"`
+}
+
+type S2Param struct {
+	fx.In
+
+	S string `name:"s2"`
+}
+
+type S2Result struct {
+	fx.Out
+
+	S string `name:"s2"`
+}
+
+func NewString1() S1Result {
+	s := string("foo")
+	return S1Result{
+		S: s,
+	}
+}
+
+func NewString2() S2Result {
+	s := string("bar")
+	return S2Result{
+		S: s,
+	}
+}
+
 func main() {
 	fx.New(
+		fx.Provide(NewString1),
+		fx.Provide(NewString2),
 		fx.Provide(NewA),
-		fx.Provide(NewB),
 		fx.Invoke(func(a *A) {
 			fmt.Println("invoke")
 		}),
@@ -17,20 +56,14 @@ func main() {
 }
 
 type A struct {
-	*B
+	S1 string
+	S2 string
 }
 
-func NewA(b *B) *A {
-	fmt.Println("create A")
+func NewA(s1 S1Param, s2 S2Param) *A {
+	fmt.Printf("create A with s1=[%s], s2=[%s]\n", s1.S, s2.S)
 	return &A{
-		B: b,
+		S1: s1.S,
+		S2: s2.S,
 	}
-}
-
-type B struct {
-}
-
-func NewB() *B {
-	fmt.Println("create B")
-	return &B{}
 }
