f99d7207353dec41fd0e3e327710980818a007fc
Leo Hung <leo.hung@idcservices.net>
2024-01-08 18:01:49 +0800

    get gcp compute instance machine type list

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # go-demo
-[Golang 取得GCP Compute VM instance](https://matthung0807.blogspot.com/2023/02/go-get-gcp-compute-vm-instance.html)
+[Golang 取得GCP執行個體機器類型 Get GCP Compute VM instance machine types](https://matthung0807.blogspot.com/2024/01/go-get-gcp-compute-vm-instance-machine-types.html)
--- main.go
+++ main.go
@@ -9,27 +9,22 @@ import (
 
 func main() {
 	ctx := context.Background()
-	service, err := compute.NewService(ctx)
+	computeService, err := compute.NewService(ctx)
 	if err != nil {
 		panic(err)
 	}
-	instancesService := compute.NewInstancesService(service)
-
 	projectId := "project-id-1"
 	zone := "asia-east1-b"
-	instanceName := "demo-instance"
-	call := instancesService.Get(projectId, zone, instanceName)
 
-	instance, err := call.Do()
+	machineTypeService := compute.NewMachineTypesService(computeService)
+	call := machineTypeService.List(projectId, zone)
+	machineTypeList, err := call.Filter("name:e2-*").Do()
 	if err != nil {
 		panic(err)
 	}
+	for _, item := range machineTypeList.Items {
+		fmt.Println(item.Name)
+		fmt.Println(item.SelfLink)
+	}
 
-	fmt.Println(instance.Id)                    // 6192712587621936341
-	fmt.Println(instance.Name)                  // demo-instance
-	fmt.Println(instance.MachineType)           // https://www.googleapis.com/compute/beta/projects/project-id-1/zones/asia-east1-b/machineTypes/e2-micro
-	fmt.Println(instance.Disks[0].DiskSizeGb)   // 20
-	fmt.Println(instance.Disks[0].DeviceName)   // demo-instance
-	fmt.Println(instance.Disks[0].Architecture) // X86_64
-	fmt.Println(instance.Disks[0].Boot)         // true
 }
