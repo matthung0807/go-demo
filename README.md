5b763186c4736645b208337b625c97d3a511cc39
Leo Hung <leo.hung@idcservices.net>
2024-01-09 10:46:08 +0800

    create gcp compute vm instance

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # go-demo
-[Golang 取得GCP執行個體機器類型 Get GCP Compute VM instance machine types](https://matthung0807.blogspot.com/2024/01/go-get-gcp-compute-vm-instance-machine-types.html)
+[Golang 建立GCP執行個體 Create GCP Compute VM instance](https://matthung0807.blogspot.com/2024/01/go-create-gcp-compute-vm-instance.html)
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 
 	compute "google.golang.org/api/compute/v1"
 )
@@ -16,15 +15,38 @@ func main() {
 	projectId := "project-id-1"
 	zone := "asia-east1-b"
 
-	machineTypeService := compute.NewMachineTypesService(computeService)
-	call := machineTypeService.List(projectId, zone)
-	machineTypeList, err := call.Filter("name:e2-*").Do()
+	instancesService := compute.NewInstancesService(computeService)
+
+	attachDisks := []*compute.AttachedDisk{
+		{
+			AutoDelete: true,
+			Boot:       true,
+			InitializeParams: &compute.AttachedDiskInitializeParams{
+				DiskName:    "instance-1-boot-disk",
+				DiskSizeGb:  20,
+				DiskType:    "projects/project-id-1/zones/asia-east1-b/diskTypes/pd-balanced",
+				SourceImage: "projects/debian-cloud/global/images/debian-11-bullseye-arm64-v20231212", // OS Image
+			},
+		},
+	}
+
+	instance := &compute.Instance{
+		Name:               "instance-1",
+		DeletionProtection: false,
+		MachineType:        "https://www.googleapis.com/compute/v1/projects/project-id-1/zones/asia-east1-b/machineTypes/e2-small",
+		NetworkInterfaces: []*compute.NetworkInterface{
+			{
+				StackType:  "IPV4_ONLY",
+				Subnetwork: "projects/project-id-1/regions/asia-east1/subnetworks/test-east1-b",
+			},
+		},
+		Disks: attachDisks,
+	}
+
+	call := instancesService.Insert(projectId, zone, instance)
+	_, err = call.Do()
 	if err != nil {
 		panic(err)
-	}
-	for _, item := range machineTypeList.Items {
-		fmt.Println(item.Name)
-		fmt.Println(item.SelfLink)
 	}
 
 }
