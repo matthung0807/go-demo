793491915e65db3b6a1adea0ecd0dfd4f0695b0a
Leo Hung <leo.hung@idcservices.net>
2024-03-07 17:20:19 +0800

    get aws cloudwatch metric statistics

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # go-demo
-[Golang 建立CloudWatch Dashboard](https://matthung0807.blogspot.com/2024/03/go-create-aws-cloudwatch-dashboard.html)
+[Golang 取得AWS CloudWatch metric統計資料](https://matthung0807.blogspot.com/2024/03/go-get-aws-cloudwatch-metric-statistics.html)
--- main.go
+++ main.go
@@ -2,29 +2,42 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/cloudwatch"
+	"github.com/aws/aws-sdk-go-v2/service/cloudwatch/types"
 )
 
 func main() {
 	ctx := context.TODO()
 	client := NewCloudWatchClient(ctx)
 
-	dashboardName := "cloudwatch-1"
-	dashboardBody := `
-{
-  "widgets": []
-}
-`
-
-	_, err := client.PutDashboard(ctx, &cloudwatch.PutDashboardInput{
-		DashboardName: aws.String(dashboardName),
-		DashboardBody: aws.String(dashboardBody),
+	output, err := client.GetMetricStatistics(ctx, &cloudwatch.GetMetricStatisticsInput{
+		MetricName: aws.String("CPUUtilization"),
+		StartTime:  aws.Time(time.Date(2024, 3, 5, 14, 0, 0, 0, time.UTC)), // 2024-03-05T14:00:00Z
+		EndTime:    aws.Time(time.Date(2024, 3, 5, 15, 0, 0, 0, time.UTC)), // 2024-03-05T15:00:00Z
+		Period:     aws.Int32(600),
+		Namespace:  aws.String("AWS/EC2"),
+		Statistics: []types.Statistic{types.StatisticAverage},
+		Dimensions: []types.Dimension{
+			{
+				Name:  aws.String("InstanceId"),
+				Value: aws.String("i-015766eb6a31d3413"),
+			},
+		},
 	})
 	if err != nil {
 		panic(err)
+	}
+
+	for _, point := range output.Datapoints {
+		fmt.Println(*point.Timestamp)
+		fmt.Println(*point.Average)
+		fmt.Println(point.Unit)
+		fmt.Println("========================================")
 	}
 }
 
