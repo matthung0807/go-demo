f8cae2090be78158f62715184530e8d5efac19b4
Leo Hung <leo.hung@idcservices.net>
2024-03-19 14:39:01 +0800

    assign

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/samber/lo"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	map1 := map[string]int{
+		"john": 33,
+		"mary": 28,
+	}
+	map2 := map[string]int{
+		"mary": 18,
+		"tony": 40,
+	}
+	resultMap := lo.Assign(map1, map2)
+	fmt.Println(resultMap)
 }
