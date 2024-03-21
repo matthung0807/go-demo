024d5767e405aec8576a18020811fbcbda8eab0d
Leo Hung <leo.hung@idcservices.net>
2024-03-21 10:21:50 +0800

    currency process messages

--- main.go
+++ main.go
@@ -5,31 +5,31 @@ import (
 )
 
 func main() {
-	messages := genMessages()
-	messageChan := make(chan string)
-	workerNum := len(messages) / 10
+	msgs := genMessages()        // 產生多筆訊息
+	msgChen := make(chan string) // 建立訊息通道(channel)
+	workerNum := len(msgs) / 10  // 要建立的goroutine(worker)數量 = 訊息數量 / 10
 	for i := 0; i < workerNum; i++ {
-		go worker(messageChan)
+		go worker(msgChen) // 建立goroutine來處理channel的訊息
 	}
-	send(messages, messageChan)
+	send(msgs, msgChen) // 將訊息發送到channel
 }
 
 func send(messages []string, messageChan chan string) {
 	for j := 0; j < len(messages); j++ {
-		messageChan <- messages[j]
+		messageChan <- messages[j] // 將訊息逐筆發送到channel
 	}
 }
 
 func worker(messageChan chan string) {
-	for message := range messageChan {
+	for message := range messageChan { // 將訊息從channel中取出。若channel中無訊息會阻塞
 		fmt.Printf("%s\n", message)
 	}
 }
 
 func genMessages() []string {
-	messages := make([]string, 0)
+	msgs := make([]string, 0)
 	for i := 0; i < 100; i++ {
-		messages = append(messages, fmt.Sprintf("message %d", i))
+		msgs = append(msgs, fmt.Sprintf("message %d", i))
 	}
-	return messages
+	return msgs
 }
