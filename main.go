4e2d95fc96a25a4b65c5623d85b360097219e060
Leo Hung <leo.hung@idcservices.net>
2024-03-21 11:46:37 +0800

    wait group

--- main.go
+++ main.go
@@ -2,29 +2,37 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 func main() {
-	msgs := genMessages()        // 產生多筆訊息
-	msgChen := make(chan string) // 建立訊息通道(channel)
-	workerNum := len(msgs) / 10  // 要建立的goroutine(worker)數量 = 訊息數量 / 10
+	msgs := genMessages()
+
+	var wg sync.WaitGroup
+	workerNum := len(msgs) / 10
+	wg.Add(workerNum)
+
+	msgChen := make(chan string)
 	for i := 0; i < workerNum; i++ {
-		go worker(msgChen) // 建立goroutine來處理channel的訊息
+		go worker(&wg, msgChen)
 	}
-	send(msgs, msgChen) // 將訊息發送到channel
+
+	send(msgs, msgChen)
+	wg.Wait()
 }
 
 func send(msgs []string, msgChen chan string) {
 	for j := 0; j < len(msgs); j++ {
-		msgChen <- msgs[j] // 將訊息逐筆發送到channel
+		msgChen <- msgs[j]
 	}
-	close(msgChen) // 訊息都送到channel後將其關閉
+	close(msgChen)
 }
 
-func worker(msgChen chan string) {
-	for msg := range msgChen { // 將訊息從channel中取出。若channel中無訊息會阻塞
-		time.Sleep(time.Second * 1) // 模擬處理每筆訊息要耗費的時間
+func worker(wg *sync.WaitGroup, msgChen chan string) {
+	defer wg.Done()
+	for msg := range msgChen {
+		time.Sleep(time.Second * 1)
 		fmt.Printf("%s\n", msg)
 	}
 }
